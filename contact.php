da493bbfdf1b08b54009c12b159ef9ea182bf0e4
fahimChowdhury234 <fahimmuntacir234@gmail.com>
2024-03-23 00:54:52 +0600

    first commit

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+// Retrieve form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+
+// Example: Perform user registration here
+// For demonstration, let's just print the received data
+echo "Received data:\n";
+echo "Name: $name\n";
+echo "Email: $email\n";
+echo "Phone: $phone\n";
+echo "Message: $message\n";
